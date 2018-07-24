57d24fe0dd9ea184f957252cb273e5ef4b922276
Moses Bosire <m.bosire@armakuni.com>
2018-07-24 09:57:52 +0100

    remove accidentally committed solutions
    Signed-off-by: Bence Monus <b.monus@armakuni.com>

--- deploy-cf.sh
+++ deploy-cf.sh
@@ -19,8 +19,6 @@ export BOSH_ALL_PROXY=socks5://localhost:12345
 
 bosh -e "$BOSH_ENVIRONMENT" update-cloud-config cf/cloud-config.yml \
   -v system_lb="$(terraform output -state=terraform/.terraform/terraform.tfstate system_lb_name)" \
-  -v app_lb="$(terraform output -state=terraform/.terraform/terraform.tfstate app_lb_name)" \
-  -v cf_ssh_lb="$(terraform output -state=terraform/.terraform/terraform.tfstate cf_ssh_lb_name)" \
   -v availability_zone="$AVAILABILITY_ZONE" \
   -v private_subnet_range="$(terraform output -state=terraform/.terraform/terraform.tfstate private_subnet_cidr)" \
   -v security_group="$(terraform output -state=terraform/.terraform/terraform.tfstate bosh_security_group_name)" \
@@ -35,7 +33,5 @@ bosh -e "$BOSH_ENVIRONMENT" -d cf deploy cf/cf-deployment/cf-deployment.yml \
   -o cf/cf-deployment/operations/use-compiled-releases.yml \
   -o cf/cf-deployment/operations/aws.yml \
   -o cf/operations/rename-disk-labels.yml \
-  -o cf/operations/multiple-diego.yml \
-  -o cf/operations/override-app-domains.yml \
   -v system_domain="sys.$STACK_NAME.training.armakuni.co.uk" \
-  -v app_domain="app.$STACK_NAME.training.armakuni.co.uk"
+ 
